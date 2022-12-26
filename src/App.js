7e27ab893e69e11b72695369e237a4cdbbdcac51
Waldemar Hermann <hermann.waldemar19995@gmail.com>
2022-12-26 12:23:36 +0100

    add props for more dynamic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import './App.css';
 import Navbar from "./components/Navbar"
-/* import Hero from "./components/Hero" */
+import Hero from "./components/Hero"
 import Card from "./components/Card"
 
-function App() {
+function App(props) {
   return (
-    // <Hero />
     <div className="App">
       <Navbar />
-      <Card />
+      <Hero />
+      <div className='card'>
+        <Card
+          img="katie-zaferes.png"
+          rating="5.0"
+          views={6}
+          title="Life lessons with Katie Zaferes"
+          price={136}
+        />
+        <Card
+          img="katie-zaferes.png"
+          rating="5.0"
+          views={6}
+          title="Life lessons with Katie Zaferes"
+          price={136}
+        />
+         <Card
+          img="katie-zaferes.png"
+          rating="5.0"
+          views={6}
+          title="Life lessons with Katie Zaferes"
+          price={136}
+        />
+      </div>
     </div>
+
   );
 }
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,19 @@
-function Card() {
+function Card(props) {
     return (
         <section className="card--section">
             <div className="card--section--img">
-                <img className="card--katie--img" src="./katie-zaferes.png" alt="Katie Zaferes picture" />
+                <img className="card--katie--img" src={props.img} alt="Katie Zaferes picture" />
                 <div className="card--section--overlay">SOLD OUT</div>
             </div>
             <div className="card--section--text">
                 <div className="card--section--starandtext">
                     <img className="card--star--img" src="./star-icon.png" alt="Star-icon" />
-                    <p>5.0</p>
-                    <p className="card--section--firstP">(6) &#x2022;USA</p>
+                    <p>{props.rating}</p>
+                    <p className="card--section--firstP">({props.views}) &#x2022;{props.country}</p>
                 </div>
-                <p className="card--section--secondP">Life lessons with Katie Zaferes</p>
+                <p className="card--section--secondP">{props.title}</p>
                 <div className="card-section--doubleP">
-                    <p className="card--section-thirdP">From $136 /</p>
+                    <p className="card--section-thirdP">From ${props.price} /</p>
                     <p>person</p>
                 </div>
             </div>
